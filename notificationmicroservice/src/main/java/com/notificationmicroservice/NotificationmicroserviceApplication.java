47dd18b6b84619ba734fbb9d3a80b8f9e3135b86
jeremiahdy55 <jeremiahjava55@gmail.com>
2025-05-20 07:14:19 -1000

    create notificationmicroservice

---
+++ notificationmicroservice/src/main/java/com/notificationmicroservice/NotificationmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.notificationmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationmicroserviceApplication.class, args);
+	}
+
+}

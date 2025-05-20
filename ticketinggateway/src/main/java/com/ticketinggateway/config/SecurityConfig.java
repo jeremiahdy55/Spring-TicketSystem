7b53355fcd1556f9c372a94207071fb9714f7328
jeremiahdy55 <jeremiahjava55@gmail.com>
2025-05-20 05:28:10 -1000

    minor readability changes in SecurityConfig.java

--- ticketinggateway/src/main/java/com/ticketinggateway/config/SecurityConfig.java
+++ ticketinggateway/src/main/java/com/ticketinggateway/config/SecurityConfig.java
@@ -17,40 +17,38 @@ public class SecurityConfig {
 	@Autowired
 	UserDetailsService userDetailsService;
 
-	private final String[] loggedInEndpoints = { "/landing", "/getHistory/*","/getTicket/*", "/ticketDetails/*", "/postTicket*", "/ticketForm"};
-	private final String[] managerEndpoints = {"/approveTicket/*", "/rejectTicket/*", "/getOpenTickets", "/getAllTickets", "/managerDashboard"};
-	private final String[] userEndpoints = {"/reopenTicket/*", "/closeTicket/*", "/getUserTickets/*", "/getActiveUserTickets/*", "/userDashboard"};
-	private final String[] adminEndpoints = {"/resolveTicket/*", "/getAssignedTickets/*", "/getActiveAssignedTickets/*", "/adminDashboard"};
+	private final String[] loggedInEndpoints = { "/landing", "/getHistory/*", "/getTicket/*", "/ticketDetails/*", "/postTicket*", "/ticketForm" };
+	private final String[] managerEndpoints = { "/approveTicket/*", "/rejectTicket/*", "/getOpenTickets", "/getAllTickets", "/managerDashboard" };
+	private final String[] userEndpoints = { "/reopenTicket/*", "/closeTicket/*", "/getUserTickets/*", "/getActiveUserTickets/*", "/userDashboard" };
+	private final String[] adminEndpoints = { "/resolveTicket/*", "/getAssignedTickets/*", "/getActiveAssignedTickets/*", "/adminDashboard" };
 	private final String deleteEndpoint = "/deleteTicket/*";
 
-	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-	@Bean 
+
+	@Bean
 	public SecurityFilterChain apiFilterChain2(HttpSecurity http) throws Exception {
 		http
 			.apply(MyCustomDsl.customDsl())
 			.flag(true).and()
 			.authorizeRequests().requestMatchers("/").permitAll().and()
-			      //.exceptionHandling().accessDeniedPage("/accessDeniedPage").and()
+			// .exceptionHandling().accessDeniedPage("/accessDeniedPage").and()
 			.authorizeRequests().requestMatchers(loggedInEndpoints).hasAnyAuthority("ADMIN", "USER", "MANAGER").and()
 			.authorizeRequests().requestMatchers(managerEndpoints).hasAnyAuthority("MANAGER").and()
 			.authorizeRequests().requestMatchers(userEndpoints).hasAnyAuthority("USER").and()
 			.authorizeRequests().requestMatchers(deleteEndpoint).hasAnyAuthority("USER", "MANAGER").and()
 			.authorizeRequests().requestMatchers(adminEndpoints).hasAnyAuthority("ADMIN").and()
+			.formLogin()
+				.loginPage("/login")
+				.defaultSuccessUrl("/landing").permitAll().and()
+			.logout()
+				.logoutSuccessUrl("/")
+			.invalidateHttpSession(true)
+			.deleteCookies("JSESSIONID")
+			.permitAll();
 
-		.formLogin()
-			.loginPage("/login")
-			.defaultSuccessUrl("/landing").permitAll().and()
-		.logout()
-			.logoutSuccessUrl("/")
-        .invalidateHttpSession(true)
-        .deleteCookies("JSESSIONID")
-        .permitAll();
-		
 		return http.build();
 	}
 }
